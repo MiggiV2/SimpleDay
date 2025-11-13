9b75a851661887ab10892dfafda55581ebd5fab6
Michael Hainz <noreply@mymiggi.de>
2025-11-13 13:36:52 +0100

    ➖ Remove unsued dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -44,7 +44,6 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="entry" options={{ headerShown: false, presentation: 'card' }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
       <Toast />
